ead8f7b69bb2ec6983b45b491d25413f25243d20
Maria Ines Casiba <mariainescasiba@yahoo.com.ar>
2024-01-02 21:44:39 -0300

    segunda pre entrega del desafio final - correcciones

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -44,25 +44,3 @@ const agregarProductoAlCarrito = async (pid) => {
         console.log("Error en agregar el Producto al Carrito! " + error);
     }
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-    const btnLogout = document.getElementById("btnLogout");
-    if (btnLogout) { 
-        btnLogout.addEventListener("click", async () => {
-            try {
-                const response = await fetch("/api/sessions/logout", {
-                    method: "POST",
-                });
-
-                if (response.status === 200) {
-                    console.log("Sesión cerrada con éxito");
-                    window.location.href = "/login";
-                } else {
-                    console.log("Error al cerrar sesión");
-                }
-            } catch (error) {
-                console.error("Error al cerrar sesión:", error);
-            }
-        });
-    }
-});
